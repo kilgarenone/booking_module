432fbc6a1cb9a857b0b5e1107d7ab7c4e28ee9d7
Kheoh Yee Wei <oldjoy@protonmail.com>
2020-03-28 14:09:08 +0800

    wrap up timeslot selection

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
 const path = require("path");
 const socket = require("socket.io");
 const storage = require("node-persist");
 const hbs = require("express-handlebars");
 const { generateThisMonthCalendar } = require("./helpers/calendar");
-const app = express();
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
@@ -21,12 +24,13 @@ app.engine(
 );
 
 const TODAY = Date.now();
-const THREE_WEEKS_FROM_TODAY = TODAY + 1000 * 60 * 60 * 24 * 21;
+const THREE_WEEKS_FROM_TODAY = TODAY + 1000 * 60 * 60 * 24 * 22;
 
 const todayObj = new Date(TODAY);
 todayObj.setHours(0, 0, 0, 0);
 const threeWeeksFromTodayObj = new Date(THREE_WEEKS_FROM_TODAY);
 threeWeeksFromTodayObj.setHours(0, 0, 0, 0);
+console.log("threeWeeksFromTodayObj:", threeWeeksFromTodayObj);
 const THIS_MONTH = todayObj.getMonth();
 const MONTH_OF_THREE_WEEKS_FROM_TODAY = threeWeeksFromTodayObj.getMonth();
 const TOTAL_MONTHS_IN_A_YEAR = 12;
@@ -63,12 +67,18 @@ for (
     thisDay === FIRST_DAY_OF_THE_WEEK ? TOTAL_WEEKDAYS_MINUS_ONE : thisDay - 1
   ).fill(0);
 
-  dates[`${monthName} ${year}`] = emptyDays.concat(
+  const days = emptyDays.concat(
     generateThisMonthCalendar(month % TOTAL_MONTHS_IN_A_YEAR, year)
   );
+
+  const chunks = [];
+  for (let i = 0; i < days.length; i += 7) {
+    chunks.push(days.slice(i, i + 7));
+  }
+  dates[`${monthName} ${year}`] = chunks;
 }
-console.log("dates:", dates);
-// console.log("cacheDates:", generateThisMonthCalendar(4, 2020));
+
+// console.log("dates:", dates);
 
 app.get("/", function(req, res, next) {
   res.render("index", {
@@ -79,12 +89,39 @@ app.get("/", function(req, res, next) {
   });
 });
 
-const server = app.listen(4000, function() {
+server.listen(4000, function() {
   console.log("listening to requests on port 4000");
 });
 
-// const io = socket(server);
+function addMinutes(time, minutes) {
+  const date = new Date(
+    new Date("01/01/2015 " + time).getTime() + minutes * 60000
+  );
 
+  const mins = date.getMinutes();
+
+  const timeSlot = [
+    date.getHours(),
+    mins.toString().length == 1 ? "0" + mins : mins
+  ];
+
+  return timeSlot;
+}
+
+function generateTimeSlots() {
+  let startTime = "8:30";
+  const interval = "30";
+  const endTime = "18:00";
+  const timeSlots = [];
+
+  while (startTime !== endTime) {
+    const time = addMinutes(startTime, interval);
+    timeSlots.push(time);
+    startTime = `${time[0]}:${time[1]}`;
+  }
+
+  return timeSlots;
+}
 // init();
 
 // async function init() {
@@ -93,23 +130,30 @@ const server = app.listen(4000, function() {
 // }
 
 // function initSocket() {
-//   io.on("connection", function(client) {
-//     client.on("join", async function(cb) {
-//       const bids = await storage.getItem("name"); // yourname
-//       console.log("bids:", bids);
+io.on("connection", function(client) {
+  client.on("getTimeSlots", async function(date, cb) {
+    // console.log("cb:", cb);
+    // console.log("date:", date);
+    cb(generateTimeSlots());
+    // const bids = await storage.getItem("name"); // yourname
+    // console.log("bids:", bids);
 
-//       cb(bids);
-//     });
+    // cb(bids);
+  });
+  client.on("confirmBooking", async function(datetime) {
+    console.log("datetime:", new Date(datetime).getHours());
+    // io.emit("chat", data);
+  });
 
-//     // listen to 'chat' emitted from client
-//     client.on("chat", async function(cb) {
-//       // then broadcast 'chat' along with data to all clients(including emitter) listening on 'chat' event
-//       const bids = await storage.getItem("name"); // yourname
+  // // listen to 'chat' emitted from client
+  // client.on("chat", async function(cb) {
+  //   // then broadcast 'chat' along with data to all clients(including emitter) listening on 'chat' event
+  //   const bids = await storage.getItem("name"); // yourname
 
-//       cb(bids);
-//       // io.emit("chat", data);
-//       // emit to other clients execept the emitter
-//       // socket.broadcast.emit("chat", data);
-//     });
-//   });
+  //   cb(bids);
+  //   // io.emit("chat", data);
+  //   // emit to other clients execept the emitter
+  //   // socket.broadcast.emit("chat", data);
+  // });
+});
 // }
--- public/auction.js
+++ public/auction.js
@@ -1,20 +1,97 @@
 const socket = io.connect("http://localhost:4000");
 
 // Get elements
-const price = document.getElementById("price");
-const auctionFeeds = document.getElementById("auction-feeds");
-const enter = document.getElementById("entry");
+const calendars = document.getElementsByClassName("calendar");
+const selectedDateEle = document.getElementById("selected-date");
 
-function displayBids(bids) {
-  auctionFeeds.innerHTML += bids;
+const options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric"
+};
+
+for (let cal of calendars) {
+  cal.addEventListener(
+    "click",
+    function(event) {
+      const ele = event.target;
+
+      if (ele.nodeName !== "BUTTON") return;
+
+      const date = ele.dataset.date;
+      selectedDateEle.innerHTML = new Date(date).toLocaleString(
+        "en-US",
+        options
+      );
+
+      socket.emit(
+        "getTimeSlots",
+        ele.dataset.date,
+        displayTimeSlots.bind(null, ele.dataset.date)
+      );
+    },
+    false
+  );
 }
 
-enter.addEventListener("click", function() {
-  // emit 'chat' event to server's socket along with some data
-  socket.emit("join", displayBids);
+const slotsContainer = document.getElementById("time-slots");
+
+slotsContainer.addEventListener("click", function(event) {
+  if (
+    event.target.classList.contains("time-slot") &&
+    !event.target.classList.contains("js-selected-time-slot")
+  ) {
+    // clean up
+    const confirmEle = document.getElementsByClassName("confirm-time-slot")[0];
+    const selected = document.getElementsByClassName(
+      "js-selected-time-slot"
+    )[0];
+    confirmEle && confirmEle.remove();
+    selected && selected.classList.remove("js-selected-time-slot");
+
+    // do work
+    event.target.classList.add("js-selected-time-slot");
+    event.target.parentNode.insertAdjacentHTML(
+      "beforeend",
+      `<button class="confirm-time-slot">Confirm</button>`
+    );
+
+    return;
+  }
+
+  if (event.target.classList.contains("confirm-time-slot")) {
+    socket.emit(
+      "confirmBooking",
+      event.target.previousSibling.dataset.datetime,
+      handleBookingSuccess
+    );
+  }
 });
 
-// client is listening on 'chat' event emitted from server, receiving data too
-socket.on("chat", function() {
-  auctionFeeds.innerHTML += data.hello;
-});
+function handleBookingSuccess() {}
+
+function displayTimeSlots(date, slots) {
+  slotsContainer.innerHTML = "";
+
+  for (const [hour, minutes] of slots) {
+    const amOrPm = hour >= 12 ? "pm" : "am";
+    const slot = `${hour > 12 ? hour - 12 : hour}:${minutes}${amOrPm}`;
+
+    slotsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div style="position:relative;white-space:nowrap"><button class="time-slot" data-datetime=${new Date(
+        `${date} ${hour}:${minutes}`
+      ).toISOString()} type="button">${slot}</button></div>`
+    );
+  }
+}
+
+// enter.addEventListener("click", function() {
+//   // emit 'chat' event to server's socket along with some data
+//   socket.emit("join", displayBids);
+// });
+
+// // client is listening on 'chat' event emitted from server, receiving data too
+// socket.on("chat", function() {
+//   auctionFeeds.innerHTML += data.hello;
+// });
